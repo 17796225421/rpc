73a4c59bc1dc9bed9e7f0efd1b99221a966a192b
aqua5201314 <aqua5201314@outlook.com>
2021-11-10 05:02:55 -0800

    处理参数

--- src/mprpcapplication.cc
+++ src/mprpcapplication.cc
@@ -1,7 +1,33 @@
 #include "mprpcapplication.h"
 
-void MprpcApplication::Init(int argc, char **argv){
+#include <iostream>
+#include <unistd.h>
+#include <string>
 
+void ShowArgsHelp(){
+    std::cout<<"format: command -i <configfile>"<<std::endl;
+}
+
+void MprpcApplication::Init(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    char c;
+    std::string config_file;
+    while((c=getopt(argc,argv,"i:"))!=-1){
+        if(c=='i'){
+            config_file=optarg;
+        }else if(c=='?'||c==':'){
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // 开始加载配置文件 rpcserver_ip=  rpcserver_port=  zookeeper_ip=  zookeeper_port=  
 }
 
 MprpcApplication &MprpcApplication::GetInstance()
