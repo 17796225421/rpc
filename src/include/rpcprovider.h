9e63249a6f81e51d758505bb665e0801ee1474f5
aqua5201314 <aqua5201314@outlook.com>
2021-11-11 00:51:39 -0800

    实现rpcprovider的rpc响应回调

--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -29,11 +29,13 @@ private:
         google::protobuf::Service*m_service;// 保存服务对象
         std::unordered_map<std::string,const google::protobuf::MethodDescriptor*>m_methodMap;// 保存服务方法
     };
-    // 存储注册成功的服务对象和其服务方法的所有信息
+    // 存储注册成功的服务对象和其服务方法信息
     std::unordered_map<std::string,ServiceInfo>m_serviceMap;
 
     // 新的socket连接回调
     void onConnection(const muduo::net::TcpConnectionPtr &);
     // 已建立连接用户的读写事件
     void onMessage(const muduo::net::TcpConnectionPtr &, muduo::net::Buffer *, muduo::Timestamp);
+    // Closure的回调操作，用于序列化rpc的响应和网络发送
+    void SendRpcResponse(const muduo::net::TcpConnectionPtr &,google::protobuf::Message*);
 };
