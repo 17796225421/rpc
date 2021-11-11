5e503af347033b46c9b7d950ae98280468c539e1
aqua5201314 <aqua5201314@outlook.com>
2021-11-11 06:09:01 -0800

    添加注册测试

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -18,6 +18,13 @@ public:
         return true;
     }
 
+    bool Register(uint32_t id, std::string name, std::string pwd)
+    {
+        std::cout << "doing local service: Register" << std::endl;
+        std::cout << "id:" << id << " name:" << name << " pwd:" << pwd << std::endl;
+        return true;
+    }
+
     /**
      * 重写基类UserServiceRpc的虚函数，下面这些方法都是框架直接调用的
      * 1. caller  ===>   Login(LoginRequest) => muduo => callee
@@ -29,28 +36,45 @@ public:
                ::google::protobuf::Closure *done)
     {
         // 框架给应用上报了请求参数LoginRequest，应用获取相应数据做本地业务
-        std::string name=request->name();
-        std::string pwd=request->pwd();
+        std::string name = request->name();
+        std::string pwd = request->pwd();
 
         // 做本地业务
-        bool login_result=Login(name,pwd);
+        bool login_result = Login(name, pwd);
 
         // 把响应写入
-        fixbug::ResultCode*code=response->mutable_result();
+        fixbug::ResultCode *code = response->mutable_result();
         code->set_errcode(0);
         code->set_errmsg("");
-        response->set_sucess(login_result);
+        response->set_success(login_result);
 
         // 执行回调操作，执行响应对象数据的序列化和网络发送(都是由框架来完成的)
         done->Run();
-
     }
 
+    void Register(::google::protobuf::RpcController *controller,
+                  const ::fixbug::RegisterRequest *request,
+                  ::fixbug::RegisterResponse *response,
+                  ::google::protobuf::Closure *done)
+    {
+        uint32_t id=request->id();
+        std::string name=request->name();
+        std::string pwd=request->pwd();
+
+        bool ret=Register(id,name,pwd);
+
+        response->mutable_result()->set_errcode(0);
+        response->mutable_result()->set_errmsg("");
+        response->set_success(ret);
+
+        done->Run();
+    }
 };
 
-int main(int argc,char **argv){
+int main(int argc, char **argv)
+{
     // 调用框架的初始化操作
-    MprpcApplication::Init(argc,argv);
+    MprpcApplication::Init(argc, argv);
 
     // provider是一个rpc网络服务对象，把UserService对象发布到rpc节点上
     RpcProvider provider;
@@ -58,6 +82,6 @@ int main(int argc,char **argv){
 
     // 执行回调操作，启动一个rpc服务发布节点，Run以后，进程进入阻塞状态，等待远程rpc调用请求
     provider.Run();
-    
+
     return 0;
 }
--- example/caller/calluserservice.cc
+++ example/caller/calluserservice.cc
@@ -23,11 +23,32 @@ int main(int argc, char **argv)
     // 一次rpc调用完成，读调用的结果
     if (0 == response.result().errcode())
     {
-        std::cout << "rpc login response:" << response.sucess() << std::endl;
+        std::cout << "rpc login response success:" << response.success() << std::endl;
     }
     else
     {
-        std::cout<<"rpc login response error: "<<response.result().errmsg()<<std::endl;
+        std::cout << "rpc login response error: " << response.result().errmsg() << std::endl;
     }
+
+    // 演示调用远程发布的rpc方法Register
+    fixbug::RegisterRequest req;
+    req.set_id(2000);
+    req.set_name("li si");
+    req.set_pwd("0987654321");
+
+    // 以同步的方式发起rpc请求，等待返回结果
+    fixbug::RegisterResponse rsp;
+    stub.Register(nullptr, &req, &rsp, nullptr);
+
+    // 一次rpc调用完成，读调用的结果
+    if (0 == rsp.result().errcode())
+    {
+        std::cout << "rpc register response success:" << rsp.success() << std::endl;
+    }
+    else
+    {
+        std::cout << "rpc register response error: " << rsp.result().errmsg() << std::endl;
+    }
+
     return 0;
 }
